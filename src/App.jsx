576acbb00a23938d5952b988de67b089b29abc2b
jlopep09 <jlopep09@estudiantes.unileon.es>
2025-04-04 14:11:05 +0200

    Creacion del proyecto con vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css"
+
+function App() {
+
+  return (
+    <>
+      <p>Bienvenido a mi web, actualmente está en desarrollo.</p>
+      <div className="w-full p-4 bg-stone-400">
+        <p>Hola</p>
+      </div>
+    </>
+  )
+}
+
+export default App
