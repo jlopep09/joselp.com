6eb5162cd317b608b1915d23fdfe9d27d44e4986
jlopep09 <jlopep09@estudiantes.unileon.es>
2025-06-02 17:05:58 +0200

    New version porfolio project

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,10 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Home from './Home.jsx'
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { BrowserRouter, Routes, Route } from "react-router";
-import "./main.css"
-
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-    primary: {
-      main: '#212121',
-    },
-  },
-});
+import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <ThemeProvider theme={darkTheme}>
-    <CssBaseline />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-</ThemeProvider>
-
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
-
-
-
-
