d5c6ed26dec5a64c245a7e92e43a001cc7996af4
Jose López <jlopep09@estudiantes.unileon.es>
2026-08-29 22:36:56 +0200

    Removed gradients

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,19 @@ import Projects from "./components/Projects";
 import Timeline from "./components/Timeline";
 
 function App() {
-  const bgClass = "bg-[#f6f7f5] bg-linear-to-t/oklab from-[#f6f7f5] via-[#edf5f2] to-[#f8f5ed] from-60% to-90%"
+  const baseBg = "#f8f5ed";
+
   return (
-    <div className="min-h-screen ">
-      <div className={" min-h-screen w-full top-0 "+bgClass}>
+    <div className="min-h-screen" style={{ backgroundColor: baseBg }}>
+      <div className="min-h-screen w-full top-0" style={{ backgroundColor: baseBg }}>
         <Navbar></Navbar>
         <About></About>
         <Timeline></Timeline>
-        <div className="bg-linear-to-b/oklab from-[#f6f7f5] via-[#eef5f2] to-[#f8f5ed] from-60% to-90%">
-        
-        <Projects></Projects>
-        <footer className="text-center py-6 text-slate-500 pt-10">
-        © {new Date().getFullYear()} - Portfolio de José Antonio López Pérez
-        </footer>
+        <div style={{ backgroundColor: baseBg }}>
+          <Projects></Projects>
+          <footer className="text-center py-6 text-slate-500 pt-10">
+            © {new Date().getFullYear()} - Portfolio de José Antonio López Pérez
+          </footer>
         </div>
         {/**
          * 
@@ -25,7 +25,6 @@ function App() {
         <Conctact></Conctact>
          */}
       </div>
-
     </div>
   );
 }
